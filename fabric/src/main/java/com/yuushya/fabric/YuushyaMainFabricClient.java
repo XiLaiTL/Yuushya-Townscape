8d0092a9b13a6f3de8cf05a6f9806efd31a83b29
Hikari <zxc17634985169@gmail.com>
2023-02-08 21:37:48 +0800

    引入Mapping前的准备

--- fabric/src/main/java/com/yuushya/fabric/YuushyaMainFabricClient.java
+++ fabric/src/main/java/com/yuushya/fabric/YuushyaMainFabricClient.java
@@ -1,27 +1,12 @@
 package com.yuushya.fabric;
 
-import com.yuushya.Blocks;
 import com.yuushya.YuushyaMainClient;
-import com.yuushya.client.ShowBlockModelFabric;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
-import net.minecraft.client.renderer.block.BlockModelShaper;
-import net.minecraft.world.level.block.state.BlockState;
 
 public class YuushyaMainFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         YuushyaMainClient.init();
         YuushyaMainClient.initItemModelPredicate();
-        ModelLoadingRegistry.INSTANCE.registerVariantProvider(
-                (resourceManager) -> (modelResourceLocation, modelProviderContext) -> {
-                    for (BlockState blockState : Blocks.SHOW_BLOCK.get().getStateDefinition().getPossibleStates()) {
-                        if (modelResourceLocation.equals(BlockModelShaper.stateToModelLocation(blockState))) {
-                            return new ShowBlockModelFabric();
-                        }
-                    }
-                    return null;
-                }
-        );
     }
 }
