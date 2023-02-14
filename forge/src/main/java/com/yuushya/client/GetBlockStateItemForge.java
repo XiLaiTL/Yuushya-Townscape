d8d2e28b6cd9dc269e85750e63b7b7e60f2abc74
Hikari <zxc17634985169@gmail.com>
2023-02-14 18:43:33 +0800

    fix

--- forge/src/main/java/com/yuushya/client/GetBlockStateItemForge.java
+++ forge/src/main/java/com/yuushya/client/GetBlockStateItemForge.java
@@ -2,12 +2,12 @@ package com.yuushya.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.yuushya.items.showblocktools.GetBlockStateItem;
-#if MC_VERSION >= "11902"
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.world.item.ItemStack;
+#if MC_VERSION >= "11902"
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 #else
 import net.minecraftforge.client.IItemRenderProperties;
@@ -38,7 +38,7 @@ public class GetBlockStateItemForge extends GetBlockStateItem {
             public BlockEntityWithoutLevelRenderer getItemStackRenderer() {
                 return new BlockEntityWithoutLevelRenderer(Minecraft.getInstance().getBlockEntityRenderDispatcher(),Minecraft.getInstance().getEntityModels()){
                     @Override
-                    public void renderByItem(@NotNull ItemStack stack, ItemTransforms.@NotNull TransformType mode, @NotNull PoseStack matrices, @NotNull MultiBufferSource vertexConsumers, int light, int overlay) {
+                    public void renderByItem(ItemStack stack, ItemTransforms.TransformType mode, PoseStack matrices, MultiBufferSource vertexConsumers, int light, int overlay) {
                         GetBlockStateItem .renderByItem(stack,mode,matrices,vertexConsumers,light,overlay);
                     }
                 };
