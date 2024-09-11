a62c118262f48f451376dcd5c10fcd91a94e4960
XiLaiTL <1272720210@qq.com>
2024-09-11 13:37:49 +0800

    test(1.16.5): AddonLoader

--- 1.16.5/common/src/main/java/com/yuushya/utils/AddonLoader.java
+++ 1.16.5/common/src/main/java/com/yuushya/utils/AddonLoader.java
@@ -7,22 +7,22 @@ import com.yuushya.collision.data.CollisionItem;
 import com.yuushya.registries.YuushyaRegistryData;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.FilePackResources;
+import net.minecraft.server.packs.FolderPackResources;
 import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.resources.FallbackResourceManager;
 import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.URL;
+import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.nio.file.Paths;
+import java.security.CodeSource;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -30,8 +30,7 @@ import java.util.stream.Stream;
 import static com.yuushya.Yuushya.MOD_ID;
 import static com.yuushya.collision.CollisionFileReader.getCollisionMap;
 import static com.yuushya.collision.CollisionFileReader.getVoxelShape;
-import static com.yuushya.registries.YuushyaRegistryConfig.addResultToRawMap;
-import static com.yuushya.registries.YuushyaRegistryConfig.mergeYuushyaRegistryBlockJson;
+import static com.yuushya.registries.YuushyaRegistryConfig.*;
 import static com.yuushya.utils.GsonTools.NormalGSON;
 
 //最主要的是读取两种文件：data/register/xxx.json, data/collision/xxx.json
@@ -40,6 +39,32 @@ public class AddonLoader {
     private static final Predicate<String> JSON_FILTER = string -> string.endsWith(".json");
     //只读取yuushya文件夹了现在
     private static final FallbackResourceManager YUUSHYA_MANAGER = new FallbackResourceManager(PackType.SERVER_DATA,MOD_ID);
+
+    private static Path classJarPath(Class<?> clazz){
+        CodeSource codeSource = clazz.getProtectionDomain().getCodeSource();
+        if(codeSource!=null){
+            URL jarPath = codeSource.getLocation();
+            String decoded = null;
+            try {
+                decoded = URLDecoder.decode(jarPath.getPath(),"UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                decoded = jarPath.getPath().replaceAll("%20","");
+            }
+            String dir = new File(decoded).getPath().replaceAll("#.+!","").replaceAll("\\.jar.+",".jar");
+            return Paths.get(dir);
+        }
+        return null;
+    }
+    public static void loadResource(Class<?> clazz){
+        Path path = classJarPath(clazz);
+        if(path!=null && Files.exists(path)){
+            try (PackResources packResource = Files.isDirectory(path)
+                    ? new FolderPackResources(path.toFile())
+                    : new FilePackResources(path.toFile())){
+                YUUSHYA_MANAGER.add(packResource);
+            }
+        }
+    }
 
     public static void loadPackResource(Path folder){
         try(Stream<Path> files = Files.list(folder).filter(ADDON_FILTER).sorted(Comparator.comparing(Path::toString)) ){
@@ -53,12 +78,17 @@ public class AddonLoader {
         } catch (IOException e) {e.printStackTrace();}
     }
 
+    private static <V> List<ResourceLocation> sortMapEntry(Collection<ResourceLocation> entrySet){
+        return entrySet.stream().sorted(
+                Comparator.comparing(ResourceLocation::getPath)
+                .thenComparing(ResourceLocation::getNamespace)).collect(Collectors.toList());
+    }
     public static void getRegister() {
-        for (ResourceLocation file : YUUSHYA_MANAGER.listResources("register", JSON_FILTER)) { try {
+        for (ResourceLocation file : sortMapEntry(YUUSHYA_MANAGER.listResources("register", JSON_FILTER))) { try {
                 for (Resource resource : YUUSHYA_MANAGER.getResources(file)) {
                     try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8));) {
                         JsonElement innerJson= new JsonParser().parse(reader);
-                        mergeYuushyaRegistryBlockJson(innerJson.getAsJsonObject().getAsJsonArray("block"));
+                        mergeYuushyaRegistryBlockClass(innerJson);
                         YuushyaRegistryData YuushyaData=NormalGSON.fromJson(innerJson, YuushyaRegistryData.class);
                         addResultToRawMap(YuushyaData);
                     }
