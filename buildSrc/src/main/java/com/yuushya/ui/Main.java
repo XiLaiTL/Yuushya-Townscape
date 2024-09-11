b55993cd11c657fa95ec70a09e8ffa7682d66cd4
XiLaiTL <1272720210@qq.com>
2024-09-11 13:37:22 +0800

    fix(buildSrc): collision read

--- buildSrc/src/main/java/com/yuushya/ui/Main.java
+++ buildSrc/src/main/java/com/yuushya/ui/Main.java
@@ -52,42 +52,42 @@ public class Main {
             collisionFileCreator.createJson();
             System.out.println("Create Success");
         }
+        else{
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            JFrame frame=new JFrame("Yuushya Control Panel");
+            frame.setSize(300,300);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            JPanel contentPane=new JPanel();
+            contentPane.setLayout(null);
+            contentPane.setBorder(new EmptyBorder(5,5,5,5));
+            frame.setContentPane(contentPane);
 
-        JFrame.setDefaultLookAndFeelDecorated(true);
-        JFrame frame=new JFrame("Yuushya Control Panel");
-        frame.setSize(300,300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel contentPane=new JPanel();
-        contentPane.setLayout(null);
-        contentPane.setBorder(new EmptyBorder(5,5,5,5));
-        frame.setContentPane(contentPane);
-
-        List<Path> paths=new ArrayList<>();
-        JButton button0 = new JButton("存档转换器");
-        button0.setBounds(30, 40,200,30);
-        button0.addActionListener((event)->{
+            List<Path> paths=new ArrayList<>();
+            JButton button0 = new JButton("存档转换器");
+            button0.setBounds(30, 40,200,30);
+            button0.addActionListener((event)->{
 //            Path self =Paths.get(AssetsAndDataCreator.class.getProtectionDomain().getCodeSource().getLocation().getPath().replaceAll("%20"," ").substring(1));
 //            JOptionPane.showMessageDialog(frame,"Load the Dependency. If there was not new window, please reopen! \n正在加载依赖，如果没有新窗口出现，请重新打开软件!");
 //            ZipReader zipReader = new ZipReader("lib",self);
 //            zipReader.filePath(Path.of("./META-INF/jars"));
-            MapConvertor.main(args);
-        });
-        JButton button = new JButton("资源生成器");
-        button.setBounds(30,80,200,30);
-        button.addActionListener((event)->{
-            AssetsAndDataCreator.main(args);
-        });
-        JButton button1 = new JButton("图片分辨率检测");
-        button1.setBounds(30,120,200,30);
-        button1.addActionListener((event)->{
-            Mode.imageSize = Mode.registerTableType("image");
-            ImageSizeChecker.main(args);
-        });
+                MapConvertor.main(args);
+            });
+            JButton button = new JButton("资源生成器");
+            button.setBounds(30,80,200,30);
+            button.addActionListener((event)->{
+                AssetsAndDataCreator.main(args);
+            });
+            JButton button1 = new JButton("图片分辨率检测");
+            button1.setBounds(30,120,200,30);
+            button1.addActionListener((event)->{
+                Mode.imageSize = Mode.registerTableType("image");
+                ImageSizeChecker.main(args);
+            });
 
-        contentPane.add(button0);
-        contentPane.add(button);
-        contentPane.add(button1);
-
-        frame.setVisible(true);
+            contentPane.add(button0);
+            contentPane.add(button);
+            contentPane.add(button1);
+            frame.setVisible(true);
+        }
     }
 }
