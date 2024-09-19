9d82782b189f0dc4c02943139198e20cb2964d4f
XiLaiTL <1272720210@qq.com>
2024-09-19 23:03:55 +0800

    feat(1.20.6): update to 1.20.6

--- 1.20.6/common/src/main/java/com/yuushya/YuushyaClient.java
+++ 1.20.6/common/src/main/java/com/yuushya/YuushyaClient.java
@@ -1,19 +1,19 @@
 package com.yuushya;
 
 import com.yuushya.collision.CollisionFileReader;
+import com.yuushya.gui.CheckScreen;
+import com.yuushya.registries.YuushyaConfig;
 import com.yuushya.registries.YuushyaRegistries;
-import com.yuushya.utils.YuushyaLogger;
+import com.yuushya.utils.CheckFileUtils;
 import dev.architectury.event.CompoundEventResult;
 import dev.architectury.event.events.client.ClientGuiEvent;
 import dev.architectury.event.events.client.ClientLifecycleEvent;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.ConfirmScreen;
-import net.minecraft.client.gui.screens.worldselection.SelectWorldScreen;
-import net.minecraft.network.chat.Component;
+import net.minecraft.client.gui.screens.TitleScreen;
 
 public class YuushyaClient {
     private static boolean openOnce =true;
     public static void onInitializeClient(){
+        YuushyaConfig.CLIENT_CONFIG.read();
 //        ReloadListenerRegistry.register(PackType.CLIENT_RESOURCES,new YuushyaResourceReloadListener());
 
         YuushyaRegistries.registerClient();
@@ -22,19 +22,22 @@ public class YuushyaClient {
             CollisionFileReader.readAllCollision();
         });
 
-        /*
-        ClientGuiEvent.SET_SCREEN.register(screen -> {
-            if(openOnce && screen instanceof SelectWorldScreen) {
-                openOnce = false;
-                return CompoundEventResult.interruptTrue(
-                        new ConfirmScreen((b)-> {
-                            Minecraft.getInstance().setScreen(screen);}, Component.translatable("menu.yuushya.confirm.title"),Component.translatable("menu.yuushya.confirm.description"))
-                );
-            } else{
-                return CompoundEventResult.pass();
-            }
-        });
-         */
+        if(YuushyaConfig.CLIENT_CONFIG.check){
+            CheckFileUtils.loadInformation();
+            ClientGuiEvent.SET_SCREEN.register(screen -> {
+                if(openOnce && screen instanceof TitleScreen) {
+                    openOnce = false;
+                    return CompoundEventResult.interruptTrue(
+                            new CheckScreen(screen)
+                            //new ConfirmScreen((b)-> {Minecraft.getInstance().setScreen(screen);}, Component.translatable("menu.yuushya.confirm.title"),Component.translatable("menu.yuushya.confirm.description"))
+                    );
+                } else{
+                    return CompoundEventResult.pass();
+                }
+            });
+        }
+
+
 //        ClientTickEvent.CLIENT_POST.register((client)->{
 //            if (client.level != null&&client.player!=null&&client.hitResult!=null &&client.hitResult.getType() == HitResult.Type.BLOCK) {
 //                BlockHitResult blockHitResult = (BlockHitResult) client.hitResult;
--- 1.20.6/common/src/main/java/com/yuushya/utils/AddonLoader.java
+++ 1.20.6/common/src/main/java/com/yuushya/utils/AddonLoader.java
@@ -5,23 +5,26 @@ import com.google.gson.JsonParser;
 import com.yuushya.Yuushya;
 import com.yuushya.collision.data.CollisionItem;
 import com.yuushya.registries.YuushyaRegistryData;
+import dev.architectury.platform.Platform;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.FilePackResources;
-import net.minecraft.server.packs.PackLocationInfo;
-import net.minecraft.server.packs.PackResources;
-import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.*;
 import net.minecraft.server.packs.repository.PackSource;
 import net.minecraft.server.packs.resources.FallbackResourceManager;
 import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.CodeSource;
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
@@ -29,8 +32,7 @@ import java.util.stream.Stream;
 import static com.yuushya.Yuushya.MOD_ID;
 import static com.yuushya.collision.CollisionFileReader.getCollisionMap;
 import static com.yuushya.collision.CollisionFileReader.getVoxelShape;
-import static com.yuushya.registries.YuushyaRegistryConfig.addResultToRawMap;
-import static com.yuushya.registries.YuushyaRegistryConfig.mergeYuushyaRegistryBlockJson;
+import static com.yuushya.registries.YuushyaRegistryConfig.*;
 import static com.yuushya.utils.GsonTools.NormalGSON;
 
 //最主要的是读取两种文件：data/register/xxx.json, data/collision/xxx.json
@@ -39,6 +41,37 @@ public class AddonLoader {
     private static final Predicate<ResourceLocation> JSON_FILTER = resourceLocation -> resourceLocation.getPath().endsWith(".json");
     //只读取yuushya文件夹了现在
     private static final FallbackResourceManager YUUSHYA_MANAGER = new FallbackResourceManager(PackType.SERVER_DATA,MOD_ID);
+
+    private static Path classJarPath(String modId, Class<?> ...clazz){
+        if(modId!=null && Platform.getModIds().contains(modId)){
+            List<Path> paths = Platform.getMod(modId).getFilePaths();
+            if(paths!=null&&!paths.isEmpty()){
+                for(Path res:paths) if(!res.toString().isEmpty()&&Files.isRegularFile(res)){
+                    return res;
+                }
+            }
+        }
+        for(Class<?> cls:clazz) {
+            CodeSource codeSource = cls.getProtectionDomain().getCodeSource();
+            if (codeSource != null) {
+                URL jarPath = codeSource.getLocation();
+                String decoded = URLDecoder.decode(jarPath.getPath(), StandardCharsets.UTF_8);
+                String dir = new File(decoded).getPath().replaceAll("#.+!", "").replaceAll("\\.jar.+", ".jar");
+                return Paths.get(dir);
+            }
+        }
+        return null;
+    }
+    public static void loadResource(String modId, Class<?> ...clazz){
+        Path path = classJarPath(modId, clazz);
+        if(path!=null && Files.exists(path)){
+            try (PackResources packResource = Files.isDirectory(path)
+                    ? new PathPackResources(new PackLocationInfo(path.getFileName().toString(), Component.empty(), PackSource.DEFAULT, Optional.empty()), path)
+                    : new FilePackResources.FileResourcesSupplier(path).openPrimary(new PackLocationInfo(path.getFileName().toString(), Component.empty(), PackSource.DEFAULT, Optional.empty()))){
+                YUUSHYA_MANAGER.push(packResource);
+            }
+        }
+    }
 
     public static void loadPackResource(Path folder){
         try(Stream<Path> files = Files.list(folder).filter(ADDON_FILTER).sorted(Comparator.comparing(Path::toString)) ){
@@ -52,12 +85,17 @@ public class AddonLoader {
         } catch (IOException e) {e.printStackTrace();}
     }
 
+    private static <V> List<Map.Entry<ResourceLocation,V>> sortMapEntry(Set<Map.Entry<ResourceLocation,V>> entrySet){
+        return entrySet.stream().sorted(
+                Comparator.comparing((Map.Entry<ResourceLocation, V> a) -> a.getKey().getPath())
+                .thenComparing(a -> a.getKey().getNamespace())).toList();
+    }
     public static void getRegister() {
-        for (Map.Entry<ResourceLocation, Resource> entry : YUUSHYA_MANAGER.listResources("register", JSON_FILTER).entrySet()) { try {
+        for (Map.Entry<ResourceLocation, Resource> entry : sortMapEntry(YUUSHYA_MANAGER.listResources("register", JSON_FILTER).entrySet())) { try {
             Resource resource = entry.getValue();
             try (BufferedReader reader = new BufferedReader(resource.openAsReader());) {
                 JsonElement innerJson= JsonParser.parseReader(reader);
-                mergeYuushyaRegistryBlockJson(innerJson.getAsJsonObject().getAsJsonArray("block"));
+                mergeYuushyaRegistryBlockClass(innerJson);
                 YuushyaRegistryData YuushyaData=NormalGSON.fromJson(innerJson, YuushyaRegistryData.class);
                 addResultToRawMap(YuushyaData);
             }
@@ -65,7 +103,7 @@ public class AddonLoader {
     }
 
     public static void getCollision(){
-        for (Map.Entry<ResourceLocation, Resource> entry : YUUSHYA_MANAGER.listResources("collision", JSON_FILTER).entrySet()) { try {
+        for (Map.Entry<ResourceLocation, Resource> entry : sortMapEntry(YUUSHYA_MANAGER.listResources("collision", JSON_FILTER).entrySet())) { try {
             ResourceLocation file = entry.getKey();
             ResourceLocation namespaceId1 =new ResourceLocation(Yuushya.MOD_ID, file.getPath().substring("collision/".length(), file.getPath().length() - ".json".length()));
             Resource resource = entry.getValue();
